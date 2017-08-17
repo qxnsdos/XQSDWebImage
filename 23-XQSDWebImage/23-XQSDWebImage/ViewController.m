bf35147b64ff5fd9b181462f2cf5261103b1761a
sdos <qiaoxianning@163.com>
2017-08-18 01:25:42 +0800

    重写main方法,指定自定义操作要执行的代码

--- 23-XQSDWebImage/23-XQSDWebImage/ViewController.m
+++ 23-XQSDWebImage/23-XQSDWebImage/ViewController.m
@@ -7,8 +7,11 @@
 //
 
 #import "ViewController.h"
+#import "DownloaderOperation.h"
 
 @interface ViewController ()
+
+@property(nonatomic,strong)   NSOperationQueue    * queue;
 
 @end
 
@@ -16,7 +19,15 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    // 创建队列
+    self.queue = [[NSOperationQueue alloc] init];
+    
+    // 创建操作
+    NSOperation *op = [[DownloaderOperation alloc] init];
+    
+    // 把操作添加到队列
+    [self.queue addOperation:op];
 }
 
 
--- 23-XQSDWebImage/23-XQSDWebImage/XQWebImage/DownloaderOperation.m
+++ 23-XQSDWebImage/23-XQSDWebImage/XQWebImage/DownloaderOperation.m
@@ -10,4 +10,13 @@
 
 @implementation DownloaderOperation
 
+// main函数 只在 队列调度操作之后才执行  该方法默认在子线程执行
+// 重写main方法的作用 : 可以在该方法里指定自定义操作要执行的代码
+- (void)main{
+    NSLog(@"main  %@",[NSThread currentThread]);
+    
+    NSData *data = [NSData dataWithContentsOfURL:nil];
+    
+}
+
 @end
