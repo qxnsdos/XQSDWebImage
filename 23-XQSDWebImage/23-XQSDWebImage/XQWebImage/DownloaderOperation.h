411fda656852eddbb1bcc2381599cfc541b55d09
sdos <qiaoxianning@163.com>
2017-08-18 01:08:07 +0800

    自定义DownloaderOperation

---
+++ 23-XQSDWebImage/23-XQSDWebImage/XQWebImage/DownloaderOperation.h
@@ -0,0 +1,13 @@
+//
+//  DownloaderOperation.h
+//  23-XQSDWebImage
+//
+//  Created by sdos on 2017/8/18.
+//  Copyright © 2017年 乔现宁. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DownloaderOperation : NSOperation
+
+@end
---
+++ 23-XQSDWebImage/23-XQSDWebImage/XQWebImage/DownloaderOperation.m
@@ -0,0 +1,13 @@
+//
+//  DownloaderOperation.m
+//  23-XQSDWebImage
+//
+//  Created by sdos on 2017/8/18.
+//  Copyright © 2017年 乔现宁. All rights reserved.
+//
+
+#import "DownloaderOperation.h"
+
+@implementation DownloaderOperation
+
+@end
